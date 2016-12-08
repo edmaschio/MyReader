48c00f42515c2ccc46b6a0db8908cc769fced23d
Ederson Maschio <edmaschio@gmail.com>
2016-12-08 09:11:04 -0200

    Melhoria na classe

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,44 @@
-import { RedditService } from '../../providers/reddit-service';
-import { Component } from '@angular/core';
-
-import { NavController, LoadingController, ActionSheetController } from 'ionic-angular';
-
-import { Http } from '@angular/http';
-
-import 'rxjs/add/operator/map';
-
+import { Component, ViewChild } from '@angular/core';
+import { NavController, LoadingController, ActionSheetController, Content } from 'ionic-angular';
 import { InAppBrowser } from 'ionic-native';
+import { RedditService } from '../../providers/reddit-service';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
+  @ViewChild(Content) content: Content;
+
   public feeds: Array<any>;
-  public noFilter: Array<any>;
-  public hasFilter: boolean = false;
   private url: string = "https://www.reddit.com/new.json";
   private olderPosts: string = "https://www.reddit.com/new.json?after=";
   private newerPosts: string = "https://www.reddit.com/new.json?before=";
 
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
+
   public searchTerm: string = '';
+  public searchTermControl: FormControl;
 
   constructor(public redditService: RedditService,
-    public navCtrl: NavController, public http: Http, 
-              public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
+              public navCtrl: NavController, 
+              public loadingCtrl: LoadingController, 
+              public actionSheetCtrl: ActionSheetController) {
 
     this.fetchContent();
+
+    this.searchTermControl = new FormControl();
+    this.searchTermControl.valueChanges.debounceTime(1000).distinctUntilChanged().subscribe(search => {
+      if (search !== '' && search) {
+        this.filterItems();
+      }
+    })
   }
 
   fetchContent(): void {
@@ -52,61 +62,50 @@ export class HomePage {
   doInfinite(infiniteScroll) {
     let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
 
-    this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
-      .subscribe(data => {
-        this.feeds = this.feeds.concat(data.data.children);
+    this.redditService.fetchData(this.olderPosts + paramsUrl).then(data => {
+      this.feeds = this.feeds.concat(data);
+      this.noFilter = this.feeds;
+      this.hasFilter = false;
 
-        this.feeds.forEach((e, i, a) => {
-          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
-            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-          }
-        })
-
-        infiniteScroll.complete();
-        this.noFilter = this.feeds;
-        this.hasFilter = false;
-      });
+      infiniteScroll.complete();
+    })
   }
 
   doRefresh(refresher) {
 
     let paramsUrl = this.feeds[0].data.name;
 
-    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
-      .subscribe(data => {
-
-        this.feeds = data.data.children.concat(this.feeds);
-
-        this.feeds.forEach((e, i, a) => {
-          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
-            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-          }
-        })
-        refresher.complete();
-      });
+    this.redditService.fetchData(this.newerPosts + paramsUrl).then(data => {
+      this.feeds = data.concat(this.feeds);
+      this.noFilter = this.feeds;
+      this.hasFilter = false;
+      refresher.complete();
+    })
   }
 
   showFilters(): void {
 
+    this.content.scrollToTop();
+
     let actionSheet = this.actionSheetCtrl.create({
-      title: 'Filter options:',
+      title: 'Opções de filtro:',
       buttons: [
         {
-          text: 'Music',
+          text: 'Música',
           handler: () => {
             this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "music");
             this.hasFilter = true;
           }
         },
         {
-          text: 'Movies',
+          text: 'Filmes',
           handler: () => {
             this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "movies");
             this.hasFilter = true;
           }
         },
         {
-          text: 'Cancel',
+          text: 'Cancelar',
           role: 'cancel',
           handler: () => {
             this.feeds = this.noFilter;
